1bb384feca80f353931af3f30ac39fa8565c5b12
syndica8e <127806332+syndica8e@users.noreply.github.com>
2023-08-23 16:53:27 -0700

    Final Solution
    Taking MAX Integer value as SMALL because you can later compare SMALLER values with this LARGEST value and get SMALL.
    
    Nextly, also take another MAX profit since you need to store MAXIMUM profit you got so far in this , since profit keeps changing every iteration of the array.

--- 121.best-time-to-buy-and-sell-stock.java
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -7,15 +7,18 @@
 // @lc code=start
 class Solution {
     public int maxProfit(int[] prices) {
-        int max_profit = 0; // test case : [ 2,4,1]
-        for ( int i =0 ; i< prices.length;i++){
-            for(int j =i+1;j<prices.length;j++){
-                int profit = prices[j] - prices[i];
-                if(profit >max_profit){
-                    max_profit = profit;
-                }
+        int small = Integer.MAX_VALUE; //[2,4,1]  [7,4,3,1,2], small =2147483647
+        int profit = 0;
+        int max_profit=0;
+        for ( int i = 0 ; i < prices.length; i++){
+            if(prices[i]<small){
+                small = prices[i]; //[0] 2 , 7 = small ;[1] small=4
             }
-        }return max_profit; //Time limit exceeded = O(n2)
+            profit = prices[i] - small; //profit = 0 ;4-2 =2;
+            if(max_profit < profit){
+                max_profit = profit;
+            }
+        } return max_profit;
 }
 }
 // @lc code=end
